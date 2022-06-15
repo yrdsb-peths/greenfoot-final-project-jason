722a65643ef4cf8412da0dbecf7aaeb218e4e7f2
joesunsunjoe <340809144@Gapps.yrdsb.ca>
2022-06-15 19:01:55 -0400

    Added Images

--- TitleScreen.java
+++ TitleScreen.java
@@ -9,7 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class TitleScreen extends World
 {
     Label titleScreen = new Label ("Modified Snake version \n This is a modified version of snake \n your tail will grow automatically,\n and your apples will disappear \n after a while. \n Don't hit yourself or the wall! \n Use WASD or arrow keys to move \n press space to start ", 40);
-       
+    GreenfootImage tile = new GreenfootImage("tile000.png");
     /**
      * Constructor for objects of class TitleScreen.
      * 
